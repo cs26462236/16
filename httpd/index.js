74704ba70c76debfc0fef749487f767f21702b80
cs26462236 <cs26462236@gmai.com>
2017-11-03 14:32:34 +0800

    修改index.js

--- httpd/index.js
+++ httpd/index.js
@@ -1,21 +1,49 @@
 'use strict';
 
- let http = require('http');
+let http = require('http');
 
- http.createServer((request, response) => {
- // 取得 node.js 的 fs 模組
- let fs = require('fs')
+http.createServer((request, response) => {
+  let fs = require('fs');
+  let postData = ''; // POST 資料
 
- fs.readFile('../htdocs/index.html', (err, data) => {
- response.writeHead(200, {
- 'Content-Type': 'text/html'
+// 利⽤ 'data' event 消耗掉 data chunk;
+// 'end' event 才會被 fired
+request.on('data', (chunk) => {
+  postData += chunk;
+
+  console.log(
+' 接收的 POST data ⽚段 k: [' + chunk + '].'
+  );
+});
+
+request.on('end', () => {
+  switch (request.url) {
+    case '/':
+      fs.readFile('../htdocs/index.html', (err, data) => {
+        if (err) {
+           console.log(' 檔案讀取錯誤');
+        }
+        else {
+          response.writeHead(200, {
+            'Content-Type': 'text/html'
+          });
+
+         response.write(data);
+         response.end();
+       }
+     });
+
+     break;
+
+   default:
+     console.log(' 未定義的存取: ' + request.url);
+
+     response.end();
+
+     break;
+   }
  });
+}).listen(8088);
 
- response.write(data);
-
- response.end();
- });
- }).listen(8088);
-
- // log message to Console
-console.log(' 伺服器啟動，連線 url: http://127.0.0.1:8088/');
+// log message to Console
+console.log(' 伺服器啓動，連線 url: http://127.0.0.1:8088/');
