3d82122ce56a937f44130378114e1ece78616d97
cs26462236 <cs26462236@gmail.com>
2017-12-08 14:18:53 +0800

    20171208

--- httpd/index.js
+++ httpd/index.js
@@ -1,68 +1,79 @@
- 'use strict';
-
- let http = require('http');
-
- const routingTable = require('./config.json');
-
-
- /**
- * 利⽤ http.ServerResponse 物件回傳檔案內容
+/**
+ *  @file       index.js
+ *  @brief      The entry function of the httpd.
+ *  @author     Yiwei Chiao (ywchiao@gmail.com)
+ *  @date       10/03/2017 created.
+ *  @date       11/17/2017 last modified.
+ *  @version    0.1.0
+ *  @copyright  MIT, (C) 2017 Yiwei Chiao
+ *  @details
  *
- * @name serve
- * @function
- * @param response - http.ServerResponse 物件
- * @param fname - 要回傳的檔案名
- * @param datatype - 回傳檔案內容的 Mine-Type
- * @returns {undefined}
+ *  The entry function of the httpd.
  */
- let serve = (response, fname, datatype) => {
- let fs = require('fs');
+'use strict';
 
- fs.readFile(fname, (err, data) => {
- if (err) {
- console.log(' 檔案讀取錯誤');
- }
- else {
- response.writeHead(200, {
- 'Content-Type': datatype
- });
+let http = require('http');
 
- response.write(data);
- response.end();
- }
- });
- };
+const routingTable = require('./config.json');
 
- http.createServer((request, response) => {
- let fs = require('fs');
+/**
+  * 利用 http.ServerResponse 物件回傳檔案內容
+  *
+  * @name serve
+  * @function
+  * @param response - http.ServerResponse 物件
+  * @param fname - 要回傳的檔案名
+  * @param datatype - 回傳檔案內容的 Mine-Type
+  * @returns {undefined}
+  */
+let serve = (response, fname, datatype) => {
+  let fs = require('fs');
 
- let postData = '';
+  fs.readFile(fname, (err, data) => {
+    if (err) {
+      console.log('檔案讀取錯誤：' + fname);
+    }
+    else {
+      response.writeHead(200, {
+        'Content-Type': datatype
+      });
 
- // 利⽤ 'data' event 消耗掉 data chunk;
- // 'end' event 才會被 fired
- request.on('data', (chunk) => {
- postData += chunk;
+      response.write(data);
+      response.end();
+    }
+  });
+};
 
- console.log(
- ' 接收的 POST data ⽚段 k: [' + chunk + '].'
- );
- });
+http.createServer((request, response) => {
+  let fs = require('fs');
 
- request.on('end', () => {
- if (request.url in routingTable) {
- let obj = routingTable[request.url];
+  let postData = '';
 
- serve(response, obj.url, obj.type);
- }
- else {
- console.log(' 未定義的存取: ' + request.url);
+  // 利用 'data' event 消耗掉 data chunk;
+  // 'end' event 才會被 fired
+  request.on('data', (chunk) => {
+    postData += chunk;
 
- response.end();
- }
- });
- }).listen(8088);
+    console.log(
+      '接收的 POST data 片段: [' + chunk + '].'
+    );
+  });
 
- // log message to Console
- console.log(' 伺服器啓動，連線 url: http://127.0.0.1:8088/');
+  request.on('end', () => {
+    if (request.url in routingTable) {
+      let obj = routingTable[request.url];
 
- // index.js
+      serve(response, obj.url, obj.type);
+    }
+    else {
+      console.log('未定義的存取: ' + request.url);
+
+      response.end();
+    }
+  });
+}).listen(8088);
+
+// log message to Console
+console.log('伺服器啓動，連線 url:  http://127.0.0.1:8088/');
+
+// index.js
